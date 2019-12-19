30b9abd2d59fd7900b9556136f3fe718c411e548
qiushaoxi <qiushaoxi@163.com>
2019-12-19 23:37:11 +0800

    1. change get ip way to query url; 2. add multi hostname; 3. if ip is same, do not update

--- index.js
+++ index.js
@@ -1,61 +1,70 @@
 const axios = require('axios')
-const publicIp = require('public-ip')
 const config = require('./config.json')
 
-async function main () {
+async function main() {
   try {
+    console.log('check ddns', new Date())
     // Load Config
-    if (!config.hostname || !config.email || !config.token) { throw Error('Config missing') }
+    if (config.hostnames.length == 0 || !config.email || !config.token || !config.queryUrl) { throw Error('Config missing') }
     const cfAuthHeaders = {
       'X-Auth-Email': config.email,
       'X-Auth-Key': config.token
     }
 
-    // Get Zone ID
-    const cfZoneIdReqUrl = `https://api.cloudflare.com/client/v4/zones?name=${encodeURI(`${config.hostname.split('.').reverse()[1]}.${config.hostname.split('.').reverse()[0]}`)}`
-    const cfZoneIdRes = await axios.get(cfZoneIdReqUrl, { headers: cfAuthHeaders })
-    if (cfZoneIdRes.data.result.length <= 0) { throw Error('Zone not found') }
-    const cfZoneId = cfZoneIdRes.data.result[0].id
-    console.log('Zone ID: ', cfZoneId)
+    let localIp = (await axios.get(config.queryUrl)).data
+    localIp = localIp.replace(/[\r\n]/g, "");
 
-    // Get DNS Record ID
-    const cfDnsIdReqUrl = `https://api.cloudflare.com/client/v4/zones/${encodeURI(cfZoneId)}/dns_records?name=${encodeURI(config.hostname)}`
-    const cfDnsIdRes = await axios.get(cfDnsIdReqUrl, { headers: cfAuthHeaders })
-    if (cfDnsIdRes.data.result.length <= 0) { throw Error('DNS record not found') }
-    const results = await Promise.all(cfDnsIdRes.data.result.map(async cfDnsRecord => {
-      console.log('DNS Record ID: ', cfDnsRecord.id)
-      let content
-      switch (cfDnsRecord.type) {
-        case 'A':
-          content = await publicIp.v4()
-          break
-        case 'AAAA':
-          content = await publicIp.v6()
-          break
-        default:
-          console.error(`DNS Record Type unsupported: ${cfDnsRecord.type}`)
+    for (const item of config.hostnames) {
+      const { hostname, proxied } = item
+      // Get Zone ID
+      const cfZoneIdReqUrl = `https://api.cloudflare.com/client/v4/zones?name=${encodeURI(`${hostname.split('.').reverse()[1]}.${hostname.split('.').reverse()[0]}`)}`
+      const cfZoneIdRes = await axios.get(cfZoneIdReqUrl, { headers: cfAuthHeaders })
+      if (cfZoneIdRes.data.result.length <= 0) { throw Error('Zone not found') }
+      const cfZoneId = cfZoneIdRes.data.result[0].id
+      // console.log('Zone ID: ', cfZoneId)
+
+      // Get DNS Record ID
+      const cfDnsIdReqUrl = `https://api.cloudflare.com/client/v4/zones/${encodeURI(cfZoneId)}/dns_records?name=${encodeURI(hostname)}`
+      const cfDnsIdRes = await axios.get(cfDnsIdReqUrl, { headers: cfAuthHeaders })
+      if (cfDnsIdRes.data.result.length <= 0) { throw Error('DNS record not found') }
+      const results = await Promise.all(cfDnsIdRes.data.result.map(async cfDnsRecord => {
+        // console.log('DNS Record ID: ', cfDnsRecord.id)
+        let content
+        switch (cfDnsRecord.type) {
+          case 'A':
+            if (cfDnsRecord.content == localIp) {
+              return { data: { success: true, message: `${cfDnsRecord.name}:${cfDnsRecord.content} do not need to change.` } }
+            }
+            content = localIp
+            break
+          // case 'AAAA':
+          //   content = await publicIp.v6()
+          //   break
+          default:
+            console.error(`DNS Record Type unsupported: ${cfDnsRecord.type}`)
+            return
+        }
+        // Update DNS Record
+        const cfPutReqUrl = `https://api.cloudflare.com/client/v4/zones/${encodeURI(cfZoneId)}/dns_records/${encodeURI(cfDnsRecord.id)}`
+        const cfPutReqData = {
+          'type': cfDnsRecord.type,
+          'name': cfDnsRecord.name,
+          content, proxied
+        }
+        return axios.put(cfPutReqUrl, cfPutReqData, { headers: cfAuthHeaders })
+      }))
+      results.forEach(result => {
+        if (!result || !result.data) {
+          console.error(`Warning: null result received, see above for error messages`)
           return
-      }
-      // Update DNS Record
-      const cfPutReqUrl = `https://api.cloudflare.com/client/v4/zones/${encodeURI(cfZoneId)}/dns_records/${encodeURI(cfDnsRecord.id)}`
-      const cfPutReqData = {
-        'type': cfDnsRecord.type,
-        'name': cfDnsRecord.name,
-        'content': content
-      }
-      return axios.put(cfPutReqUrl, cfPutReqData, { headers: cfAuthHeaders })
-    }))
-    results.forEach(result => {
-      if (!result || !result.data) {
-        console.error(`Warning: null result received, see above for error messages`)
-        return
-      }
-      if (result.data.success === true) {
-        console.log(`DNS Record update success: `, JSON.stringify(result.data, undefined, 2))
-      } else {
-        console.error(`DNS Record update failed: `, JSON.stringify(result.data.errors, undefined, 2))
-      }
-    })
+        }
+        if (result.data.success === true) {
+          console.log(`DNS Record update success: `, JSON.stringify(result.data, undefined, 2))
+        } else {
+          console.error(`DNS Record update failed: `, JSON.stringify(result.data.errors, undefined, 2))
+        }
+      })
+    }
   } catch (e) {
     console.error(e)
   }
--- readme.md
+++ readme.md
@@ -1,32 +1,72 @@
-Cloudflare DNS Updater
-===
+# Cloudflare DDNS
+
 NodeJS Script for updating CloudFlare DNS record of servers with dynamic IP.
+
 ## Dependencies
-* axios
-* public-ip
+
+- axios
+
 ## Usage
+
 ### Setup
+
 ```sh
-$ git clone git@github.com:chihimng/cloudflare-dns-updater.git
-$ cd cloudflare-dns-updater
+$ git clone https://github.com/qiushaoxi/cloudflare-ddns-cn.git
+$ cd cloudflare-ddns-cn
 $ npm install
 ```
+
 ### Configuration
-Fill in your hostname, email and api token in `config.json`
+
+```sh
+mv config.sample.json config.json
+```
+Fill in your hostnames, email and api token in `config.json`
+
 #### Sample JSON
+
 ```json
 {
-  "hostname": "homelab.example.com",
+  "hostnames": [
+    {
+      "hostname": "baidu.com",
+      "proxied": true
+    },
+    {
+      "hostname": "google.com",
+      "proxied": true
+    }
+  ],
   "email": "user@example.com",
-  "token": "c2547eb745079dac9320b638f5e225cf483cc5cfdda41"
+  "token": "c2547eb745079dac9320b638f5e225cf483cc5cfdda41",
+  "queryUrl": "http://members.3322.org/dyndns/getip"
 }
 ```
+
 ### Run
+
 ```sh
 $ node index.js
 ```
+
 ### Schedule auto updates
+
 You may use crontab to schedule updates: (every 15 minutes in sample below)
+
 ```
 */15 * * * * /usr/local/bin/node /path/to/repo/index.js
+```
+
+Or you can use pm2 tool to schedule updates
+
+Change frequency in pm2.json
+
+```
+pm2 start pm2.json
+```
+
+stop pm2 schedule
+
+```
+pm2 delete  pm2.json
 ```
